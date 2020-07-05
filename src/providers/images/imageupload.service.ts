5519d91ad4b33baf68db0610dd1f748ce6f2386a
Enriketo <anaelmarin86@gmail.com>
2020-07-05 11:32:56 -0500

    cambios de nuevo

--- src/providers/images/imageupload.service.ts
+++ src/providers/images/imageupload.service.ts
@@ -6,8 +6,8 @@ import * as multerS3 from 'multer-s3';
 const hcmediabucket = process.env.hcmediabucket;
 const s3 = new AWS.S3();
 AWS.config.update({
-  accessKeyId: process.env.AKIAJ2QR5SNDWN2IA7MQ,
-  secretAccessKey: process.env.IgwwCOwCTygTYxfVm6Tpt1ucyEuqgdKrOvKcGO2l,
+  accessKeyId: process.env.AKIAJ74ZOAAUU3T2DTXA,
+  //secretAccessKey: process.env.Rwgr7KiEcXfHv3E+DoJ6Q/nFHysMVlDte0M8hfqM,
 });
 
 @Injectable()
