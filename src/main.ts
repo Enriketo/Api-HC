85c807e6f02aaa4ac6e7df72eebd06ebc6984a87
Enriketo <55071663+Enriketo@users.noreply.github.com>
2025-01-06 21:00:27 -0500

    Update main.ts
    CORS

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { ValidationPipe } from '@nestjs/common';
+import { cors } from "cors";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,11 +16,30 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup("api", app, document);
 
-  app.enableCors({
-    origin: ['http://localhost:8000', 'http://3.137.39.122:8000'], // Agrega las URLs que necesitas
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos que quieres permitir
-    allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
-  });
+  //CORS 
+  const whitelist = [
+    "https://www.hotcompanyapp.company",
+    "https://hotcompanyapp.company",
+  ];
+
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  };
+  
+  app.use(cors(corsOptions));
+
+//  app.enableCors({
+//    origin: ['http://localhost:8000', 'http://3.137.39.122:8000'], // Agrega las URLs que necesitas
+//    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos que quieres permitir
+//    allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos
+//  });
+  
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(8000);
