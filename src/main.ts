b160a2ae577488e9c7719487ce586d23779f5388
Enriketo <55071663+Enriketo@users.noreply.github.com>
2025-03-22 20:33:22 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   //CORS 
   const whitelist = [
     '*',
-    'https://juansebastiandiazv.github.io',
+//    'https://juansebastiandiazv.github.io',
     'https://www.hotcompanyapp.company',
     'https://hotcompanyapp.company',
 //    'https://juansebastiandiazv.github.io/Hot_workers_Hotels',
